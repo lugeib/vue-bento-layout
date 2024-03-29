257162e8b59110bab1cc21aacadda389ad7de0c5
LUGEIB <luis_astec2@hotmail.com>
2024-03-29 02:11:44 -0700

    Changed package name

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import vueBento from './vue-bento'
+import vueBentoLayout from './vue-bento-layout'
 
 const app = createApp(App);
-app.use(vueBento);
+app.use(vueBentoLayout);
 app.mount('#app')
